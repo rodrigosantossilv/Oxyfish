664bcc1ef00e3f5cab0fb3a831a9e199f0451eff
Victor Ferreira <victorferreira9990@gmail.com>
2024-05-20 09:16:33 -0300

    ...

--- express.js
+++ express.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const usuarioRoutes = require('../routes/usuarioRoutes');
+const usuarioRoutes = require('./routes/usuarioRoutes');
 
 const app = express();
 const port = 3000;
 
-app.use(express.json()); 
-app.use(usuarioRoutes); 
+app.use(express.json()); // Middleware para parsear JSON
+app.use(usuarioRoutes);  // Uso das rotas de usuário
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
